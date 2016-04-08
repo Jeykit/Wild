9cd62f95640c468987a16c31c493613bdbb3cdf1
Adaman <adaman@Adamans-Mac.local>
2016-04-08 03:21:49 -0700

    first commit

---
+++ Wild/MenuView/View/MUCustomTextField.swift
@@ -0,0 +1,21 @@
+//
+//  MUCustomTextField.swift
+//  Wild
+//
+//  Created by Adaman on 4/8/16.
+//  Copyright © 2016 Adaman. All rights reserved.
+//
+
+import UIKit
+
+class MUCustomTextField: UITextField {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
